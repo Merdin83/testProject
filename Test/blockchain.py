8b0c65437597a50209dada3d4094f429feffcf02
Merdin83 <XXXXX@XXXXX.XX>
2018-09-15 23:09:15 +0200

    Test was removed

--- Test/blockchain.py
+++ Test/blockchain.py
@@ -3,7 +3,7 @@ import json
 from time import time
 
 
-#source: https://hackernoon.com/learn-blockchains-by-building-one-117428612f46  #Test
+#source: https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
 class Blockchain(object):
     
     def __init__(self):
